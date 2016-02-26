eb20d19226dc41dcc72b1ded830ed7a5ebc317ad
Rob Coward <rob@jive-videos.net>
2016-02-26 10:46:59 +0000

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
-# vcd-healthcheck
+# vCloud Director Healthcheck
 This is a collection of scripts intended to be run again a vCloud Director VDC and provide feedback and recommendations for any mis-configured or non-optimal configurations.
+
+## Installation
+Download the latest release of the healthcheck from GitHub.
+
+## Usage
+Run the command:
+```
+vcd-healthcheck
+```
+You will be prompted to enter your Username, Password and Organisation ID. 
+
+Note: Your Username is not your email address used to login to the Skyscape Portal. You must retrieve the Username and Organisation ID to use from the [Skyscape Portal API Page](https://portal.skyscapecloud.com/user/api).
+
+Optionally, you can set your user credentials as the following environment variables to prevent being prompted.
+```
+VCLOUD_USERNAME=1111.1.111111
+VCLOUD_PASSWORD=VerySecret
+VCLOUD_ORG=1-1-11-111111
+```
+
+License and Authors
+-------------------
+Authors:
+  * Rob Coward (rcoward@skyscapecloud.com)
+
+Copyright 2016 Skyscape Cloud Services
+
+Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
---
+++ vcd-healthcheck.go
@@ -0,0 +1,164 @@
+package main
+
+import (
+    "bufio"
+    "fmt"
+    "net/url"
+    "os"
+    "strings"
+    "time"
+
+    "github.com/hmrc/vmware-govcd"
+    "github.com/howeyc/gopass"
+    // "github.com/davecgh/go-spew/spew"
+    "github.com/olekukonko/tablewriter"
+    "github.com/fatih/color"
+    types "github.com/hmrc/vmware-govcd/types/v56"
+)
+
+type Config struct {
+    User     string
+    Password string
+    Org      string
+    Href     string
+    VDC      string
+    Insecure bool
+}
+
+func (c *Config) Client() (*govcd.VCDClient, error) {
+    u, err := url.ParseRequestURI(c.Href)
+    if err != nil {
+        return nil, fmt.Errorf("Unable to pass url: %s", err)
+    }
+
+    vcdclient := govcd.NewVCDClient(*u, c.Insecure)
+    org, vcd, err := vcdclient.Authenticate(c.User, c.Password, c.Org, c.VDC)
+    if err != nil {
+        return nil, fmt.Errorf("Unable to authenticate: %s", err)
+    }
+    vcdclient.Org = org
+    vcdclient.OrgVdc = vcd
+    return vcdclient, nil
+}
+
+func CheckVM(client *govcd.VCDClient, s types.QueryResultVMRecordType) ([]string, error) {
+  if s.VAppTemplate == true {
+    return nil, nil
+  }
+
+  ReturnRow := false
+  red := color.New(color.FgRed).SprintFunc()
+
+  VM, err := client.FindVMByHREF(s.HREF)
+  if err != nil {
+      return nil, fmt.Errorf("Unable to load VM: %s", err)
+  }
+
+  HWVersion := fmt.Sprintf("%d", s.HardwareVersion)
+  if s.HardwareVersion != 9 { 
+    HWVersion = red(HWVersion)
+    ReturnRow = true
+  }
+
+  NetworkDevice := "Unknown"
+  for _,v := range VM.VM.VirtualHardwareSection.Item {
+    if v.ResourceType == 10 {
+      NetworkDevice = v.ResourceSubType
+    }
+  }
+  if NetworkDevice != "VMXNET3" {
+    NetworkDevice = red(NetworkDevice)
+    ReturnRow = true
+  }
+
+  SnapshotCount := 0
+  OldSnapshots := 0
+  // CurrentTime := time.now()
+  for _, snapshot := range VM.VM.Snapshots.Snapshot {
+    SnapshotCount += 1
+    Created, _ :=time.Parse("RFC3339", snapshot.Created)
+    if time.Now().Sub(Created).Hours() > (7 * 24) {
+      OldSnapshots += 1
+    }
+  }
+  SnapshotString := fmt.Sprintf("%d", OldSnapshots)
+
+  if OldSnapshots > 0 {
+    SnapshotString = red(SnapshotString)
+    ReturnRow = true
+  }
+
+  if ReturnRow == true {
+    return []string{s.Name, HWVersion, NetworkDevice, SnapshotString}, nil
+  } else {
+    return nil, nil
+  }
+}
+
+
+func main() {
+
+  var User string = ""
+  var maskedPassword []byte 
+  var Org string = ""
+
+  reader := bufio.NewReader(os.Stdin)
+  if os.Getenv("VCLOUD_USERNAME") == "" {
+    fmt.Print("Enter your Username: ")
+    User, _ = reader.ReadString('\n')
+  }
+
+  if os.Getenv("VCLOUD_PASSWORD") == "" {
+    fmt.Print("Enter your Password: ")
+    maskedPassword, _ = gopass.GetPasswdMasked()
+  }
+
+  if os.Getenv("VCLOUD_ORG") == "" {
+    fmt.Print("Enter your Organisation ID: ")
+    Org, _ = reader.ReadString('\n')
+  }
+
+  config := Config{
+        User:     strings.TrimSpace(User),
+        Password: strings.TrimSpace(string(maskedPassword)),
+        Org:      strings.TrimSpace(Org),
+        Href:     "https://api.vcd.portal.skyscapecloud.com/api",
+        VDC:      "DevOps Demo Env (IL2-TRIAL-BASIC)",
+    }
+
+  client, err := config.Client() // We now have a client
+  if err != nil {
+      fmt.Println(err)
+      os.Exit(1)
+  }
+  // fmt.Printf("Org URL: %s\n", client.OrgHREF.String())
+
+  results, err := client.Query(map[string]string{"type": "vm"})
+  fmt.Printf("Found %d VMs ... processing\n", int(results.Results.Total))
+
+  table := tablewriter.NewWriter(os.Stdout)
+  table.SetHeader([]string{"VM", "H/W Version", "Network Device", "Old Snapshots"})
+  table.SetBorder(false)
+
+  TableRows := 0
+  for _, s := range results.Results.VMRecord {
+
+    row, err := CheckVM(client, *s)
+    if err != nil {
+      fmt.Println(err)
+      os.Exit(1)
+    }
+
+    if row != nil {
+      table.Append(row)
+      TableRows += 1
+    }
+  }
+
+  if TableRows > 0 {
+    table.Render()
+  } else {
+    fmt.Printf("No problems found.")
+  }
+}
+
