115a90137b80923043e8b400ba93f3b6f806188e
Rob Coward <rob@jive-videos.net>
2016-02-26 14:04:20 +0000

    Moving build process into its own shell script

---
+++ build.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+set -ev
+
+OS="linux windows"
+ARCH="amd64"
+
+echo "Getting build dependencies"
+go get . 
+go get -u github.com/golang/lint/golint
+
+echo "Ensuring code quality"
+go vet ./...
+golint ./...
+go test -v -check.v ./...
+
+for GOOS in $OS; do
+    for GOARCH in $ARCH; do
+        arch="$GOOS-$GOARCH"
+        binary="bin/vcd-healthcheck.$arch"
+        echo "Building $binary"
+        GOOS=$GOOS GOARCH=$GOARCH go build -o $binary
+    done
+done
+
+if [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then
+	go get github.com/laher/goxc
+	goxc -t
+	goxc bump
+	goxc
+fi
